3cccdfe7d5ef64a0f8986e6770b5feef902e7c71
georgijbudik <georgijbudik@gmail.com>
2023-12-04 14:26:33 +0100

    finish hw

--- models/user.js
+++ models/user.js
@@ -28,6 +28,7 @@ const userSchema = new Schema(
       type: String,
       default: null,
     },
+    avatarURL: String,
   },
   { versionKey: false }
 );
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ctrl = require("../../controllers/auth");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const router = express.Router();
 
@@ -13,5 +13,12 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch("/", authenticate, ctrl.updateSubscription);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatarURL"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
