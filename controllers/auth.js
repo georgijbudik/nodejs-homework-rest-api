08c00c0525bfce756c5632a25fbe1e1a7370dcf0
georgijbudik <georgijbudik@gmail.com>
2023-12-06 11:47:20 +0100

    finish hw

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,6 +88,7 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
+  await resizeFile(tempUpload);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
 
