98d4f441030f4942298b246d38dc4ea77da8390b
georgijbudik <georgijbudik@gmail.com>
2024-02-10 11:13:54 +0100

    add PORT to env

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const { DB_HOST } = process.env;
+const { PORT, DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
     });
   })
